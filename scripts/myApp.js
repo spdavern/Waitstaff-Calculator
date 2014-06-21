1772bd1d553baa708659b6663df6ae4b9202dd1f
spdavern <sean@lizandsean.com>
2014-06-21 10:42:37 -0700

    Assignment fullfilled.

--- scripts/myApp.js
+++ scripts/myApp.js
@@ -1,25 +1,57 @@
 angular.module('myApp', [])
-	.constant('VERSION', "0.2")
+	.constant('VERSION', "1.0")
 	.run(function(VERSION, $rootScope) {
 		$rootScope.version = VERSION;
 	})
-	.controller('madLibsCtrl', function($scope) {
-		$scope.data = {gender: 'male'}
+	.controller('CalcCtrl', function($scope) {
+		$scope.data = {
+			defaultTaxRate: 6.5,
+			defaultTipPercentage: 15,
+			tipTotal: 0,
+			mealCount: 0,
+			AvgTipPerMeal: 0
+		};
+		$scope.data.taxRate = $scope.data.defaultTaxRate;
+		$scope.data.tipPercentage = $scope.data.defaultTipPercentage;
+
 		$scope.submit = function() {
-			if($scope.data.gender == 'male') {
-				$scope.data.heShe = "he";
-				$scope.data.hisHer = "his";
-				$scope.data.himHer = "him"
-			} else {
-				$scope.data.heShe = "she";
-				$scope.data.hisHer	= "her";
-				$scope.data.himHer = "her"
-			};
-			if($scope.myForm.$valid) {
-				$scope.showResult = true;
-			} else {
-				$scope.showResult = false;
-			};
+			if($scope.data.mealTotal != 0 && !isNaN($scope.data.mealTotal)) {
+				$scope.data.tipTotal += $scope.data.tip;
+				$scope.data.mealCount += 1;
+				$scope.data.AvgTipPerMeal = $scope.data.tipTotal/$scope.data.mealCount;
+				$scope.resetMeal();
+			}
+		}
+		$scope.resetDefaults = function() {
+			$scope.data.taxRate = $scope.data.defaultTaxRate;
+			$scope.data.tipPercentage = $scope.data.defaultTipPercentage;
+			$scope.resetMeal();
+		}
+		$scope.resetMeal = function() {
+			$scope.data.mealPrice = undefined;
+			document.getElementById("Price").focus();
+		}
+		$scope.calcSubtotal = function() {
+			var subtotal = $scope.data.mealPrice * (1 + $scope.data.taxRate/100);
+			subtotal = Math.round(subtotal*100)/100;
+			$scope.data.subTotal = subtotal;
+			return subtotal;
+		}
+		$scope.calcTipAmt = function() {
+			var tip = $scope.data.mealPrice * $scope.data.tipPercentage/100;
+			tip = Math.round(tip*100)/100;
+			$scope.data.tip = tip;
+			return tip;
+		}
+		$scope.calcMealTotal = function(){
+			$scope.data.mealTotal = $scope.data.subTotal + $scope.data.tip;
+			return $scope.data.mealTotal;
+		}
+		$scope.reset = function() {
+			$scope.data.mealPrice = null;
+			$scope.data.tipTotal = 0;
+			$scope.data.mealCount = 0;
+			$scope.data.AvgTipPerMeal = 0;
 		}
 	}
 );
